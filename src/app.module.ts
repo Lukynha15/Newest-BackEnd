a950ddf1939ff1fd413d7e7b1e6f442b4af28052
Lukynha15 <Lukynha48@gmail.com>
2025-12-08 20:10:06 -0300

    bug no comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PostsModule } from './posts/posts.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, PostsModule],
+  imports: [ConfigModule.forRoot(), UserModule, PostsModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/comment/comment.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { UpdateCommentDto } from './dto/update-comment.dto';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class CommentService {
+  constructor(private prisma: PrismaService) {}
+  async create(createCommentDto: CreateCommentDto) {
+    const commentPost = await this.prisma.comment.create({data: createCommentDto})
+    return commentPost;
+  }
+
+  findAll() {
+    return `This action returns all comment`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} comment`;
+  }
+
+  update(id: number, updateCommentDto: UpdateCommentDto) {
+    return `This action updates a #${id} comment`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} comment`;
+  }
+}
