65d1894ab7a3350d3a087caf2aa0efd02cdd5205
Lukynha15 <Lukynha48@gmail.com>
2025-12-08 21:05:06 -0300

    fix comment

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
-import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Controller('comment')
 export class CommentController {
@@ -12,19 +11,9 @@ export class CommentController {
     return this.commentService.create(createCommentDto);
   }
 
-  @Get()
-  findAll() {
-    return this.commentService.findAll();
-  }
-
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.commentService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
-    return this.commentService.update(+id, updateCommentDto);
   }
 
   @Delete(':id')
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,29 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCommentDto } from './dto/create-comment.dto';
-import { UpdateCommentDto } from './dto/update-comment.dto';
 import { PrismaService } from 'src/prisma.service';
+import { CreateCommentDto } from './dto/create-comment.dto';
 
 @Injectable()
 export class CommentService {
   constructor(private prisma: PrismaService) {}
   async create(createCommentDto: CreateCommentDto) {
-    const commentPost = await this.prisma.comment.create({data: createCommentDto})
+    const commentPost = await this.prisma.comment.create({
+      data: {
+        content: createCommentDto.content,
+        postId: createCommentDto.postId,
+      },
+    });
     return commentPost;
   }
 
-  findAll() {
-    return `This action returns all comment`;
-  }
-
   findOne(id: number) {
-    return `This action returns a #${id} comment`;
-  }
-
-  update(id: number, updateCommentDto: UpdateCommentDto) {
-    return `This action updates a #${id} comment`;
+    return this.prisma.comment.findUnique({ where: { id } });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} comment`;
+    return this.prisma.comment.delete({ where: { id } });
   }
 }
